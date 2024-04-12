ef47947fabd3e81e97a5038906bbd0474cb23f05
LeoPeixinho <Leonardooliveirap@outlook.com>
2024-04-12 01:37:04 -0300

    Third Commit - Laço de Repetição e finalização do código

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-//Criação da Classe do nosso herói 
 class Hero {
-    constructor(name, age, type){
-        this.name = name;
-        this.age = age;
-        this.type = type;
-        }
-    traveling(){
-        return console.log (`Nosso Herói ${this.name} saiu para uma aventura em busca de Desafios`) 
-    }
+  constructor(name, age, type){
+    this.name = name;
+    this.age = age;
+    this.type = type;
+    } 
 }       
-        const hero1 = new Hero ("Eddard Starck", 20, "Mago")
-        function atack(type) {
-        if (hero1.type === "Mago"){
-        return (`O ${hero1.type} atacou usando Magia`)
-        } else if (hero1.type === "Guerreiro"){
-        return (`O ${hero1.type} atacou usando Espada`)    
-        } else if (hero1.type === "Monge"){
-        return (`O ${hero1.type} atacou usando Artes Marcias`)
-        } else if (hero1.type === "Ninja")
-        return (`O ${hero1.type} atacou usando shuriken`)
-        }
-         const fight = atack(hero1);
-         console.log (fight) 
 
+const hero1 = new Hero ("Eddard Starck", 20, "Guerreiro")
+
+function atack(type) {
+  if (hero1.type === "Mago"){
+        return (`O ${hero1.type} atacou usando Magia`)
+  } else if (hero1.type === "Guerreiro"){
+        return (`O ${hero1.type} atacou usando Espada`)    
+  } else if (hero1.type === "Monge"){
+        return (`O ${hero1.type} atacou usando Artes Marcias`)
+  } else if (hero1.type === "Ninja"){
+        return (`O ${hero1.type} atacou usando shuriken`)
+  }
+ }
+ console.log(`O nosso Herói ${hero1.name} saiu de Winterfell em busca de aventura.`)
+            
+let i = 1;
+let enemies = 10;
+for(i; i < enemies; i++){
+  console.log(`O Herói está em batalha contra o inimigo ${i}`);
+}
+const fight = atack(hero1)
+console.log (fight)
+                      
+         
+         
                
